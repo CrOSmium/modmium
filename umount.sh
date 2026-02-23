42b2dc5e9a3dae9290c8234b41413bdaaa2ccf0e
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-02-22 22:51:36 -0600

    thanks kxtz <3

--- umount.sh
+++
@@ -1,9 +0,0 @@
-# QOL scripts to test modmium.bin. DELETE BEFORE RELEASE.
-
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root, elevating with sudo..."
-	 sudo "$0" "$@"
-	 exit $?
-fi
-umount mnt && rm -rf mnt
-losetup -D
