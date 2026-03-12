34d446feb03dbbe42ba4d273385e321f6950e47f
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-03-12 15:37:25 -0500

    fix user removal

--- mod-files/usr/bin/remove-user.sh
+++ mod-files/usr/bin/remove-user.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+B='\033[1;36m' 
+G='\033[1;32m' 
+Y='\033[38;5;220m'
+R='\033[38;5;203m'
+N='\033[0m'    
+D='\033[1;90m'
+
 echo -ne "If your school has a custom sign-in page, you can use this to remove an account without powerwashing (requires reboot). \
-${D}(Ctrl+C to cancel)
+${D}(Ctrl+C to cancel) \
 ${G}Enter target email: ${N}"
 read -rep "" email
-hash=$(cryptohome --action=obfuscate_user --user=$email)
-if [[ -d /home/.shadow/$hash ]]; then
-  echo -e "${G}User directory found! Preparing to delete on reboot...${N}"
-  echo $hash >/.delete_user
-  echo -e "${G}Done! Reboot when ready...${N}"
-  sleep 2
-  return
-else
-  echo -e "${R}User directory not found :(${N} \
-Exiting..."
-  sleep 2
-  return
-fi 
+echo $email >/.delete_user
+echo -e "${G}Done setting up, reboot to finish!${N}"
+sleep 2
+return
