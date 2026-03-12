64bbff565ca956aab0b066f0bc0b61e523213ba3
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-03-12 15:39:33 -0500

    fix newlines

--- mod-files/usr/bin/remove-user.sh
+++ mod-files/usr/bin/remove-user.sh
@@ -6,9 +6,9 @@ R='\033[38;5;203m'
 N='\033[0m'    
 D='\033[1;90m'
 
-echo -ne "If your school has a custom sign-in page, you can use this to remove an account without powerwashing (requires reboot). \
-${D}(Ctrl+C to cancel) \
-${G}Enter target email: ${N}"
+echo -e "If your school has a custom sign-in page, you can use this to remove an account without powerwashing (requires reboot). \
+${D}(Ctrl+C to cancel)"
+echo -ne "${G}Enter target email: ${N}"
 read -rep "" email
 echo $email >/.delete_user
 echo -e "${G}Done setting up, reboot to finish!${N}"
