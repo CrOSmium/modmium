6affa59ea3ea2130aace2e670700bcd1f1cd5d99
xz8f <163360403+crossjbly@users.noreply.github.com>
2026-03-13 13:01:33 -0600

    add custom bootsplashes, also I added the image back as a placeholder for testing, will add downloading a backup from the internet later

--- mod-files/usr/bin/modify-bootsplash.sh
+++ mod-files/usr/bin/modify-bootsplash.sh
@@ -18,8 +18,7 @@ echo -e "| Bootsplash replacer                          |"
 echo -e "| -------------------------------------------- |"
 echo -e "| Replaces the stock chromeos bootsplash       |"
 echo -e "+##############################################+${N}"
-echo -e "${D}${UN}This script assumes the replacement png is /root/.modmium_bootsplash.png
-If this is not the case, hit (Ctrl+C) to exit.${N}${RUN}"
+echo ""
 
 replace() {
 	for splashframe in $(find /usr/share/chromeos-assets/images_100/percent -mindepth 1 -name 'boot_splash_frame*.png'); do
@@ -31,6 +30,22 @@ replace() {
 	echo -e "${G}Replaced bootsplash!${N}"
 }
 
+replace_custom() {
+	echo -e "${G}enter the FULL path to the custom image!${N}"
+	read -rep " > " custom_img_path
+	if [ -f "$custom_img_path" ]; then
+		for splashframe in $(find /usr/share/chromeos-assets/images_100/percent -mindepth 1 -name 'boot_splash_frame*.png'); do
+		mv $splashframe "$splashframe".old
+		if [[ $splashframe == *"00.png" ]]; then
+			cp "$custom_img_path" $splashframe
+		fi
+	done
+	echo -e "${G}Replaced bootsplash!${N}"
+	else
+    	echo -e "${R}The image $custom_img_path does not exist! make sure you have the path right${N}"
+	fi
+}
+
 restore() {
 	for splashframe in $(find /usr/share/chromeos-assets/images_100_percent -mindepth 1 -name 'boot_splash_frame*.png'); do
 		mv "$splashframe".old "$splashframe"
@@ -38,19 +53,30 @@ restore() {
 	echo -e "${G}Restored bootsplash!${N}"
 }
 
-echo "${G}
-1. Replace bootsplash
-2. Restore normal bootsplash
-${N}"
+download_backup() {
+	# I am hosting the stock bootsplash images on https://dl.xz8f.gay/chromeos_bootsplash.zip
+	# just need to download, unzip, copy files
+	echo "not implemented yet"
+}
+
+echo -e "${G}1. Replace bootsplash with modmium bootsplash${N}"
+echo -e "${G}2. Replace bootsplash with custom image${N}"
+echo -e "${G}3. Restore normal bootsplash{$N}"
+# echo -e "${G}4. Download stock bootsplash and save to backup${N}"
+
 
 read -rep "Choose an option: " choice
 
 if [ "$choice" == "1" ]; then
 	replace
 elif [ "$choice" == "2" ]; then
+	replace_custom
+elif [ "$choice" == "3" ]; then
 	restore
+elif [ "$choice" == "4" ]; then
+	download_backup
 else
-	echo -e "Invalid option, select either 1 (restore) or 2 (replace)"
+	echo -e "Invalid option, select either 1 (replace), 2 (replace with custom image) or 3 (restore)" # or 4 (download)
 fi
 sleep 2
 return
