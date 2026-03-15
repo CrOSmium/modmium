6cc55d3269aa6f7635f816f6c573d33ec612230f
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-03-15 10:36:02 -0500

    grammar and bug fixes

--- mod-files/usr/bin/modify-bootsplash.sh
+++ mod-files/usr/bin/modify-bootsplash.sh
@@ -12,64 +12,69 @@ D='\033[1;90m'
 P='\033[1;35m'
 UN='\033[4m' #underline
 RUN='\033[24m' #reset underline
+fail() {
+	echo -e "$1"
+	sleep 3
+	return
+}
+cros_assets="/usr/share/chromeos-assets/images_100_percent"
 
 echo -e "${P}+##############################################+"
-echo -e "| Bootsplash replacer                          |"
+echo -e "| Bootsplash Replacer                          |"
 echo -e "| -------------------------------------------- |"
-echo -e "| Replaces the stock chromeos bootsplash       |"
+echo -e "| Replaces the stock ChromeOS bootsplash       |"
 echo -e "+##############################################+${N}"
 echo ""
 
 replace() {
-	for splashframe in $(find /usr/share/chromeos-assets/images_100_percent -mindepth 1 -name 'boot_splash_frame*.png'); do
+	for splashframe in $(find $cros_assets -mindepth 1 -name 'boot_splash_frame*.png'); do
 		mv $splashframe "$splashframe".old
 		if [[ $splashframe == *"00.png" ]]; then
 			cp /root/.modmium_bootsplash.png $splashframe
 		fi
 	done
-	echo -e "${G}replaced bootsplash!${N}"
+	echo -e "${B}Replaced bootsplash!${N}"
 }
 
 replace_custom() {
-	echo -e "${G}enter the FULL path to the custom image!${N}"
+	echo -e "${G}Enter the FULL path to the custom image!${N}"
 	read -rep " > " custom_img_path
 	if [ -f "$custom_img_path" ]; then
-		for splashframe in $(find /usr/share/chromeos-assets/images_100_percent -mindepth 1 -name 'boot_splash_frame*.png'); do
+		for splashframe in $(find $cros_assets -mindepth 1 -name 'boot_splash_frame*.png'); do
 		mv $splashframe "$splashframe".old
 		if [[ $splashframe == *"00.png" ]]; then
 			cp "$custom_img_path" $splashframe
 		fi
 	done
-	echo -e "${G}replaced bootsplash!${N}"
+	echo -e "${B}Replaced bootsplash!${N}"
 	else
-    	echo -e "${R}the image $custom_img_path does not exist! make sure you have the path right${N}"
+    	fail "${R}The image $custom_img_path does not exist! Make sure you have the path right!${N}"
 	fi
 }
 
 restore() {
-	for splashframe in $(find /usr/share/chromeos-assets/images_100_percent -mindepth 1 -name 'boot_splash_frame*.png'); do
-		mv "$splashframe".old "$splashframe"
+	for splashframe in $(find $cros_assets -mindepth 1 -name 'boot_splash_frame*.old'); do
+		mv "$splashframe" "${splashframe%.*}.png"
 	done
-	echo -e "${G}restored bootsplash!${N}"
-	echo -e "${Y}note: if the bootsplash is missing or it didn't restore, use the \"download stock bootsplash\" option" # lol just incase something happens ig
+	echo -e "${B}Restored bootsplash!${N}"
+	echo -e "${Y}Note: if the bootsplash is missing or it didn't restore, use the \"Download stock bootsplash\" option${N}" # lol just incase something happens ig
 }
 
 download_backup() {
-	cros_assets="/usr/share/chromeos-assets/images_100_percent"
-	echo -e "${G}downloading stock bootsplash${N}"
+	echo -e "${G}Downloading stock bootsplash...${N}"
 	curl -LO https://dl.xz8f.gay/chromeos_bootsplash.zip
-	echo -e "${Y}unzipping${N}"
+	echo -e "${Y}Unzipping...${N}"
 	bsdtar -xf chromeos_bootsplash.zip
-	echo -e "${Y}creating new backup${N}" # should this say something different? idk
-	for i in $(seq -f "%02g" 0 30); do
-    	mv "boot_splash_frame${i}.png" "$cros_assets/boot_splash_frame${i}.old" # I could probably do something like mv "boot_splash_frame*.png" but I dont wanna bother with that rn
+	echo -e "${Y}Creating backup...${N}" # should this say something different? idk
+	for splashframe in boot_splash_frame*.png; do
+    	mv $splashframe $cros_assets/$splashframe.old # I could probably do something like mv "boot_splash_frame*.png" but I dont wanna bother with that rn
 	done
-	echo -e "${Y}cleaning up${N}"
+	echo -e "${Y}Cleaning up!${N}"
 	rm chromeos_bootsplash.zip
-	echo -e "${G}done! use option 3 (Restore stock bootsplash) to restore the stock bootsplash${N}"
+	echo -e "${B}Done! Use option 3 (Restore stock bootsplash) to restore the stock bootsplash${N}"
 }
 
-echo -e "${G}1. Replace bootsplash with modmium bootsplash${N}"
+echo -e "${G}1. Replace bootsplash with modmium bootsplash (the one used during building)${N}"
 echo -e "${G}2. Replace bootsplash with custom image${N}"
 echo -e "${G}3. Restore stock bootsplash${N}"
 echo -e "${G}4. Download stock bootsplash and save to backup${N}"
@@ -88,5 +93,5 @@ elif [ "$choice" == "4" ]; then
 else
 	echo -e "Invalid option, select either 1 (replace), 2 (replace with custom image) or 3 (restore) or 4 (download stock)"
 fi
-read -p "press enter to go back to the menu" temp
-return
+
+fail "Returning..." # not truly a "fail" but it's less clunky and more consistent with how other scripts go back to menu
