de223bf5ed6d5455a661f7991042fc157efaac64
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-03-24 23:36:43 -0500

    more bugfix

--- mod-files/usr/bin/modify-bootsplash.sh
+++ mod-files/usr/bin/modify-bootsplash.sh
@@ -47,11 +47,15 @@ Open the Files app to see them."
 	echo -ne "Enter one of the filenames above: "
 	read -rep "" bootsplash
 	bootsplash=/bootsplash/$bootsplash
+	local shouldExit=true
 	for image in $(find /bootsplash -mindepth 1 -name '*.png' | sort); do
-		if [[ "$bootsplash" != "$image" ]]; then
-			fail "${R}Invalid filename...${N}"
+		if [[ "$bootsplash" == "$image" ]]; then
+			shouldExit=false
 		fi
 	done
+	if [[ $shouldExit == true ]]; then
+		fail "${R}Invalid filename...${N}"
+	fi
 }
 
 # checks if image was built with bootsplashes
