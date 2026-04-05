02ccb27bd1480a83ea3c4525d344683eecb43fd9
xz8f <163360403+crossjbly@users.noreply.github.com>
2026-04-05 17:53:18 -0500

    ok I think this is good 🎉🙏🙏🙏

--- mod-files/usr/bin/modify-bootsplash.sh
+++ mod-files/usr/bin/modify-bootsplash.sh
@@ -16,7 +16,8 @@ RUN='\033[24m' #reset underline
 fail() {
 	echo -e "$1"
 	for downloadsDir in $(find /home/user/*/MyFiles/Downloads -maxdepth 0); do
-		rm -rf ${downloadsDir}/bootsplashes
+		# rm -rf ${downloadsDir}/bootsplashes
+		sudo chown -R chronos:chronos "$downloadsDir/bootsplashes"
 	done # we do this because you can't delete them in the files app for some reason? idk man
 	sleep 3
 	return
