47858e16473d1bf3c6a6b02aa3cb8bebc3b2f3a9
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-04-07 14:01:59 -0500

    I haven't tested this, don't use it unless you want to test it.

---
+++ mod-files/root/.emergencyreset_DO_NOT_USE.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# written by dmd, this is a WIP
+
+B='\033[38;5;45m'
+G='\033[38;5;46m'
+Y='\033[38;5;220m'
+R='\033[38;5;203m'
+P='\033[38;5;135m'
+N='\033[0m'
+D='\033[1;90m'
+UN='\033[4m' #underline
+RUN='\033[24m' #reset underline
+
+if [ $(id -u) -ne 0 ]; then
+    echo "Please run this script as root. You can do so by using 'sudo su'."
+    exit 1
+fi
+fail(){
+	echo -e "$1"
+	sleep 1
+	exit 1
+}
+checkWP(){
+	writeprotect=$(flashrom --wp-status 2>&1 | grep "disabled")
+	if [[ $writeprotect == *"disabled"* ]]; then
+		echo -e "FWWP is currently ${R}DISABLED${N}, continuing..."
+	else
+		echo -e "FWWP is currently ${G}ENABLED${N}, checking for wp range..."
+		wprange=$(flashrom --wp-status 2>&1 | grep -E "range: start=0x[0-9a-f]+, len=0x00000000")
+		if [[ $wprange != "" ]]; then
+			fail "WP range is set to 0,0 but you must fully disable FWWP before continuing."
+		else
+			fail "WP range is still set, please disable your FWWP by following this guide: ${G}https://crosmium.dev/FWWP${N}"
+		fi
+	fi
+
+
+echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to normal IF you lost your backup, however, it is very destructive and can brick your device."
+echo -e "${R}This will update your firmware and possibly re-keyroll you, are you ${UN}sure${RUN} you want to continue?${N} [y/N]"
+read -re
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+chromeos-firmwareupdate --mode=factory --force --gbb_flags=0xa0b1
+fi
