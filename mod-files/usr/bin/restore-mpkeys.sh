016d0af20d5bbeab13db9bf42b3308fc15f870a0
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-04-07 18:06:49 -0500

    working on emergency revert submenu

--- mod-files/usr/bin/restore-mpkeys.sh
+++ mod-files/usr/bin/restore-mpkeys.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # created by dmd
-# THIS WILL BE ADDED INTO MOSH EVENTUALLY (likely in the future emergency revert)
+
 B='\033[38;5;45m'
 G='\033[38;5;46m'
 Y='\033[38;5;220m'
@@ -54,18 +54,26 @@ unkeyroll(){
 }
 
 main(){
+    clear
     echo -e "Firmware write protection is [${R}disabled${N}], continuing..."
-    echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to normal if you lost your backup, however, it is very destructive and can brick your device."
-	echo -e "${R}${UN}ONLY${RUN} use this if you lost your backup, and need to revert to factory FW at all costs${N}"
+    echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to normal if you lost your backup.\nhowever, this is very destructive and can brick your device."
+    echo ""
+	echo -e "${R}${UN}ONLY${RUN} use this if you lost your backup${N}, and need to revert to factory FW ${UN}at all costs${RUN}${N}"
     echo -e "${R}This will update your firmware and possibly re-keyroll you, are you ${UN}sure${RUN} you want to continue?${N} [y/N]"
     read -re
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        chromeos-firmwareupdate --mode=factory --force --gbb_flags=0xa0b1
-        vpd -i RW_VPD -d "dev_firmware" # factory fwupdate should wipe this, but just in case
-
-        if [[ "$board" == "dedede" || "$board" == "corsola" || "$board" == "nissa" ]]; then
-            unkeyroll
-        fi
+        echo -e "${R}are you ${UN}really${RUN}, REALLY sure? ${N}[${R}y${N}/N]"
+        read -re 
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                echo -e "Restoring MPkeys..."
+                sleep 2
+                chromeos-firmwareupdate --mode=factory --force --gbb_flags=0xa0b1
+                vpd -i RW_VPD -d "dev_firmware"
+                sleep 0.75
+                if [[ "$board" == "dedede" || "$board" == "corsola" || "$board" == "nissa" ]]; then
+                    unkeyroll
+                fi
+            fi
     fi
 fail "Exiting..."
 }
