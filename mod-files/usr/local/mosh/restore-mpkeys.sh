d590b0cad667da7d05dbeb2e7db34cf4638d4151
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-04-07 20:12:44 -0500

    im about to test this one wish me luck

--- mod-files/usr/local/mosh/restore-mpkeys.sh
+++ mod-files/usr/local/mosh/restore-mpkeys.sh
@@ -39,7 +39,7 @@ checkWP(){
 }
 
 unkeyroll(){
-    echo -e "You are currently ${R}keyrolled${N}, would you like to be unkeyrolled? [y/N]"
+    echo -e "You are currently ${R}keyrolled${N}, would you like to be unkeyrolled?\n(unkeyrolling breaks AP-RO verification) [y/N]"
     read -re
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         futility gbb -s --flash --recoverykey="/root/.recoverykeys/$board.vbpubk"
@@ -56,7 +56,7 @@ unkeyroll(){
 main(){
     clear
     echo -e "Firmware write protection is [${R}disabled${N}], continuing..."
-    echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to normal if you lost your backup.\nhowever, this is very destructive and can brick your device."
+    echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to factory if you lost your backup."
     echo ""
 	echo -e "${R}${UN}ONLY${RUN} use this if you lost your backup${N}, and need to revert to factory FW ${UN}at all costs${RUN}${N}"
     echo -e "${R}This will update your firmware and possibly re-keyroll you, are you ${UN}sure${RUN} you want to continue?${N} [y/N]"
@@ -65,17 +65,36 @@ main(){
         echo -e "${R}are you ${UN}really${RUN}, REALLY sure? ${N}[${R}y${N}/N]"
         read -re 
             if [[ $REPLY =~ ^[Yy]$ ]]; then
-                echo -e "Restoring MPkeys..."
+                echo -e "Restoring MPkeys, ${G}please connect your device to power (if you haven't already)${N}"
                 sleep 2
-                chromeos-firmwareupdate --mode=factory --force --gbb_flags=0xa0b1
+                flashrom -r /pre-restorefw.rom # secretly backup the fw in case something fails
+                # before you think about removing the weird backup, i put it there because the longer it takes the more time someone will have to realize they missed something (like their battery being low, or not plugged in)
+                echo -e "Restoring firmware, ${R}do not turn off your device${N}!"
+                chromeos-firmwareupdate --mode=factory --force
                 vpd -i RW_VPD -d "dev_firmware"
                 sleep 0.75
-                if [[ "$board" == "dedede" || "$board" == "corsola" || "$board" == "nissa" ]]; then
-                    unkeyroll
+                echo -e "Do you want your device to be 'factory'? [y/N]\nThis means your device will pass APROV, but will be keyrolled (if it can be), and GBB flags will be 0x0.\n(if you press [N], the post restore will continue)"
+                read -re
+                if [[ $REPLY =~ ^[Yy]$ ]]; then
+                    echo -e "Your device is now factory! Modmium will not boot after your device reboots, so please make sure you have a regular recovery image for [$board] on hand."
+                    sleep 3
+                    fail "Skipping unkeyroll/gbbflags and exiting..."
+                else
+                    echo -e "Do you want to set GBB flags to '0xa0b1' or leave them as default? [y/N]"
+                    read -re
+                    if [[ $REPLY =~ ^[Yy]$ ]]; then
+                        futility gbb -s --flash --flags=0xa0b1 || /usr/share/vboot/bin/set_gbb_flags.sh 0xa0b1
+                    fi
+                    if [[ "$board" == "dedede" || "$board" == "corsola" || "$board" == "nissa" ]]; then
+                        unkeyroll
+                    fi
                 fi
             fi
+            echo -e "Your device is now on MPkeys! Modmium will not boot after your device reboots, so please make sure you have a regular recovery image for [${UN}$board${RUN}] on hand."
+            sleep 3
+            fail "Exiting..."
     fi
-fail "Exiting..."
+    fail "Exiting..."
 }
 
 main
