95b19a9e014767a1d482f7c0b3811e18f7d3dc15
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-04-07 20:14:46 -0500

    oops

--- mod-files/usr/local/mosh/restore-mpkeys.sh
+++ mod-files/usr/local/mosh/restore-mpkeys.sh
@@ -55,6 +55,7 @@ unkeyroll(){
 
 main(){
     clear
+    checkWP
     echo -e "Firmware write protection is [${R}disabled${N}], continuing..."
     echo -e "This script is a work in progress, it should return you to MPkeys and restore your firmware to factory if you lost your backup."
     echo ""
