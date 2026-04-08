7dfd1ed0dba361510d9cb0fb979b8ea8fb956059
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-04-07 22:53:37 -0500

    update my credits

--- build-image.sh
+++ build-image.sh
@@ -23,7 +23,7 @@ credits(){
 	echo -e "\
 Credits:
 ${R}mariahscarycarey: ${P}Lead developer; laid out everything (prior to kxtz) conceptually, made image builder, worked on policy-test-tool with lxrd, MANY small changes and fixes.${N}
-\033[38;5;78mdmd: The TUI guy; made MOSH and devfw installer.${N}
+\033[38;5;78mdmd: Project lead; made MOSH, devfw stuff, mpkeys manager, a bunch of small changes.${N}
 \033[38;5;126mkxtzownsu: Made the buildcharge package, updater, and did code review to make sure we weren't skidding.${N}
 ${Y}lxrd: Discovered policy-test-tool, worked with mariah to get it working.${N}
 \033[38;5;93mxz8f/crossjbly: Helped with custom bootsplashes.${N}
