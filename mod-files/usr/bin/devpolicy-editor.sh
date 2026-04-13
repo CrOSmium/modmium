c8b17c140aa0e0e81813cec7998916683d8f0b22
SPIRAME <143002735+SPIRAME@users.noreply.github.com>
2026-04-13 08:14:19 -0400

    Might fix lag

--- mod-files/usr/bin/devpolicy-editor.sh
+++ mod-files/usr/bin/devpolicy-editor.sh
@@ -302,6 +302,7 @@ submenu(){
 		read -rsn1 key
 		if [[ "$key" == $'\x1b' ]]; then
 			read -rsn2 -t 0.05 keyseq
+			while read -rsn1 -t 0.01 _; do :; done
 			[[ -z "$keyseq" ]] && break
 			case "$keyseq" in
 				'[A') subSelectedIndex=$(((subSelectedIndex - 1 + numOptions) % numOptions)) ;;
@@ -316,6 +317,7 @@ submenu(){
 				break
 			else
 				editJsonValue "${keys[$subSelectedIndex]}"
+				loadData
 				clear
 			fi
 		fi
@@ -349,6 +351,7 @@ full_menu(){
 		read -rsn1 key
 		if [[ "$key" == $'\x1b' ]]; then
 			read -rsn2 -t 0.05 keyseq
+			while read -rsn1 -t 0.01 _; do :; done
 			case "$keyseq" in
 				'[A') mainSelectedIndex=$(((mainSelectedIndex - 1 + mainNumOptions) % mainNumOptions)) ;;
 				'[B') mainSelectedIndex=$(((mainSelectedIndex + 1) % mainNumOptions)) ;;
