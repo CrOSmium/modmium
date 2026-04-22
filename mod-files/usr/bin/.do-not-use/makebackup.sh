e028cd48c0b91d00cdce180dc3e9d2ee0b6f6dd0
xz8f <163360403+xz8f@users.noreply.github.com>
2026-04-22 13:54:02 -0600

    ok

--- mod-files/usr/bin/.do-not-use/makebackup.sh
+++ mod-files/usr/bin/.do-not-use/makebackup.sh
@@ -1,2 +1,8 @@
 # this file *should* be able to reconstruct a firmware backup by using https://github.com/coreboot/coreboot/blob/main/util/chromeos/crosfirmware.sh and fixing the vpd and gbb
 # ill work on this when I have time
+
+
+call_codename() {
+    hwidp="$1"
+    # ok I need to switch to normal chromeos to do some testing for getting the HWID. This function will just get the HWID, then remove whatever comes after the space and anything after "-" if there is one 
+}
