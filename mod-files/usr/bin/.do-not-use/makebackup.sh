3bab6f41ea6f1740b2a17caa805d1a4338a7861e
xz8f <163360403+xz8f@users.noreply.github.com>
2026-04-22 14:20:06 -0600

    this is so stupid lmao ill make it automatic when I get home

--- mod-files/usr/bin/.do-not-use/makebackup.sh
+++ mod-files/usr/bin/.do-not-use/makebackup.sh
@@ -2,7 +2,31 @@
 # ill work on this when I have time
 
 
-get_codename() {
-    hwidp="$1"
+# reminders:
+# futility gbb -s --flags=0x80b1 filename.bin
+# futility gbb -s --hwid="HWID" filename.bin
+
+
+#get_codename() {
+#    hwidp="$1"
     # ok I need to switch to normal chromeos to do some testing for getting the HWID. This function will just get the HWID, then remove whatever comes after the space and anything after "-" if there is one 
+#}
+
+# ^^^ nvm I didnt wanna have to do setup
+get_userhwid_img() {
+    read -p "enter your device codename as it is on https://cros.tech: " usercodename
+    if [[ "$usercodename" == "" ]]; then
+        echo "please enter a codename."
+        sleep 5
+        fail # :whale:
+    fi
+    if [[ ! -f "crosfirmware.sh" ]]; then
+        echo "getting crosfirmware.sh..."
+        curl -fLO https://raw.githubusercontent.com/coreboot/coreboot/36f0b1257009e6acd314d319226afdc2fe7f234c/util/chromeos/crosfirmware.sh || { # thanks con lol
+        echo "failed to download, make sure you are on a network with githubusercontent.com unblocked"
+        exit 1
+    }
+    fi
+    bash crosfirmware.sh "$usercodename"s 
 }
+# TODO: actually modify the image and ask if the user wants to flash
