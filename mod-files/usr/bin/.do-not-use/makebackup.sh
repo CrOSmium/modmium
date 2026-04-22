61bfce6c18b34146851f91b9554ed1be858dc635
xz8f <163360403+xz8f@users.noreply.github.com>
2026-04-22 13:47:07 -0600

    .

---
+++ mod-files/usr/bin/.do-not-use/makebackup.sh
@@ -0,0 +1,2 @@
+# this file *should* be able to reconstruct a firmware backup by using https://github.com/coreboot/coreboot/blob/main/util/chromeos/crosfirmware.sh and fixing the vpd and gbb
+# ill work on this when I have time
