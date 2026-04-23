bfd3f4e94a36694c735cb4ccc3a12d58a5d1651c
xz8f <163360403+xz8f@users.noreply.github.com>
2026-04-23 10:15:36 -0600

    ok wait I think bash doesnt have spaces idk I'm used to python

--- mod-files/usr/bin/.do-not-use/makebackup.sh
+++ mod-files/usr/bin/.do-not-use/makebackup.sh
@@ -12,7 +12,7 @@ get_hwid() { # this will be useful later
 }
 
 get_codename() {
-    hwid = "$(get_hwid)"
+    hwid="$(get_hwid)"
     echo "${hwid%% *}"
 }
 # ^^^ nvm I didnt wanna have to do setup
