d0a3fc3b89d1fbafd79033cc170717932b8699a5
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-04-23 23:16:16 -0500

    stop hardcoding menu items, use arrays

--- mod-files/usr/bin/mosh-misc.sh
+++ mod-files/usr/bin/mosh-misc.sh
@@ -1,48 +1,30 @@
 #!/bin/bash
-
 # written by DMD
-
 
 # -- Pre TUI init --
 stty -echo 
-source /usr/lib/libmosh.sh
+. /usr/lib/libmosh.sh
+. /usr/lib/mosh-functions.sh
 
 # -- MAIN SCRIPT --
 tput civis # :whale:
 milestone
 
 menu_reset() {
-	options=("1) Modify Bootsplash" "2) Toggle Enrollment" "3) Open Cr3nroll" "4) ${R}Emergency Revert${N}" "5) Install Nix" "6) Go back")
-    num_options=${#options[@]}
+	options=("Modify Bootsplash" "Toggle Enrollment" "Open Cr3nroll" "${R}Emergency Revert${N}" "Install Nix" "Go back")
+	functions=("modsplash" "toggleEnrollment" "cr3nroll" "erevert" "prenix" "return")
+	num_options=${#options[@]}
 }
 menu_reset
 
 selector() {
-	sel="${options[$selected_index]}"
-
-	case "$sel" in
-		1*)
-			runscript "bash /usr/bin/modify-bootsplash.sh"
-			;;
-		2*)
-			runscript "bash /usr/bin/toggle-enrollment.sh"
-			;;
-    3*)
-        runscript "bash /usr/bin/cr3nroll.sh"
-        ;;
-    4*)
-        runscript "bash /usr/bin/emergencyrevert.sh"
-        ;;
-    5*)
-        exec /usr/bin/nix-preinstall.sh
-        ;;
-	6*)
-		stty echo
-		tput cnorm
-		clear
-		exec /usr/bin/crosh
-	esac
+	for option in ${!options[@]}; do
+		if [[ $selected_index == $option ]]; then
+			${functions[$option]}
+		fi
+	done
 }
+
 clear
 full_menu
 tput cnorm
