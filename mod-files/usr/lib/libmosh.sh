147e8b73a805dca51eadf35d24c8f5579fefd83b
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-05-14 13:49:38 -0500

    bug: remove as_system breaking MILESTONE

--- mod-files/usr/lib/libmosh.sh
+++ mod-files/usr/lib/libmosh.sh
@@ -25,7 +25,7 @@ N=$'\033[0m'
 D=$'\033[1;90m'
 UN=$'\033[4m' #underline
 RUN=$'\033[24m' #reset underline
-MILESTONE=$(as_system "grep MILESTONE /etc/lsb-release | cut -d= -f2" | tr -d '\r')
+MILESTONE=$(grep MILESTONE /etc/lsb-release | cut -d= -f2 | tr -d '\r')
 
 # STOLEN CODE FROM BR0KER TO GET MILESTONE :3
 get_largest_cros_blockdev() {
