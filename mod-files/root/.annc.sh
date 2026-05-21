7227356e837a755755a25dac68fe1b8869fc5423
xz8f <163360403+xz8f@users.noreply.github.com>
2026-05-21 10:07:48 -0600

    remove bloated comments!
    ldfskahasdflkjfhdslkfdsjhiehiuhiuhsdflkjehkjdhlkjdhfsajkdfnkasefkjlsdnaksjefnjaslnfeoifufnuioenfiesndidnjnasesiduofhndlkjnsakjfnsioefbeosiufhiuahsduiofsiejffioesjfsiojsdiosjefiurghoierugiuwhoiughogieuhgiougheruiohgiujgsdhdflkjahdkjlhdflkjdfhklfjdhkjlnclkjsdnkldjsnlkncsdkvjbvdbsiuebuigiuwghiuhdiuhgiuesghieuwhgiudshgjkensgiuedshifhwoeihesdiuhfspiehfoihwpwh3eouirh498y980735809780973098375407830987095287450982750983570985730528735098732409857039475089340987509827340983524709538735098732509873520938245709erwurqiewrupoqwieruporeqwurpoiurqiopuewqiopuqrpoeiurpoiruwpoeriuweroipuqrewpoeiurwofjhksaldf;jdflkjfd;lfkjdf;ljflkndfkldcncndsklsjdndsfkljdsfla;jfdslkjfdsf;dskj;dflkdjsf;lfsjdnieunsdiujoeifnsdiuieusfhidsjfoefnsdiuhfeiopjoisfjoisjsoidfjoisejgdofiuhfoiesuhfoiuhsfoihsfoiudhfoiuhefoiuhiogheiogeuhiueghegiuhegiuhgeiueghiuhegiugheghiuhihfdkgjnfgkjnfkjnlksjlkjghsgkldhslgdkjhkjlhiouhiouerhgiufdsnoisudhifjeioudbgjnfieofkjoihreiuhgrpghiogrjeiognrhunugidfsngijnginhiafiuhdsniufdhsnidfsahuafidsuhndfsiohfdsofdssihdfsuifdusfdhdfuoisdfuhidfshuhdfsshdfuihufdihfdusaisdfnsfhdusnhufnhodfiuadfsfdfhmsdfuiomdifsoifdushmfiouhoiuehdifubhsuihguheroiughiufdhgjeuhioerdfshgaipguhriduhphrgriuh3guohoiasdhifh

--- mod-files/root/.annc.sh
+++ mod-files/root/.annc.sh
@@ -5,18 +5,18 @@ else
   vpd -i RW_VPD -s "modmium_annc_timer"="$(date +%s)"
 fi
 get_annc() {
-  u="https://modmium.dev/announcement.txt" # sorry dmd I forgot the format u wanted ill figure it out later. also: can we do like a list or something and add checks for things later ig
+  u="https://modmium.dev/announcement.txt"
   m="$(curl -fsSL --max-time 5 "$u")"
-  echo -e "$m" # can we do colors with this :thinking:
+  echo -e "$m"
 }
-tung_tung_tung_sahur() { # I ddont feel like coming up with proper variable names im tired
+tung_tung_tung_sahur() {
   t=$(vpd -i RW_VPD -g modmium_annc_timer)
   n=$(date +%s)
   if (( now - t > 10800 )); then
     get_annc
     vpd -i RW_VPD -s "modmium_annc_timer"="$(date +%s)"
   else
-    : # is this like "pass" in python right
+    :
   fi
 }
 tung_tung_tung_sahur
