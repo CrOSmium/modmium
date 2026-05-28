da5da55136e261d6660f6bcfeb1750b20f131767
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-05-28 15:23:17 -0500

    just to be safe

--- modmium.sh
+++ modmium.sh
@@ -128,7 +128,7 @@ installCros() {
 	source .venv/bin/activate
 	pip install requests &>/dev/null
 	curl -Lo "https://modmium.dev/tools/stream.py" /root/stream.py
-	/root/stream.py --recovery-url "${recoveryUrl}" --kern-output "${installKern}" --root-output "${installRoot}" || fail "${R}Failed to install ChromeOS, refusing to change boot order, exiting...${N}"
+	python /root/stream.py --recovery-url "${recoveryUrl}" --kern-output "${installKern}" --root-output "${installRoot}" || fail "${R}Failed to install ChromeOS, refusing to change boot order, exiting...${N}"
 	rm -rf .venv
 	# thanks lxrd for that python script btw
 
