f56dc068a358a556f9b972fd4bb664c2a34cba61
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-05-27 19:31:21 -0500

    adjust credits

--- build-image.sh
+++ build-image.sh
@@ -36,10 +36,10 @@ trap cleanup EXIT
 credits(){
   cat <<EOF | xargs -0 echo -ne
 Credits:
-${R}mariahscarycarey: ${P}Lead developer; made image builder, device policy editor frontend, did most bugfixing, MANY small changes to other code.${N}
-\033[38;5;78mdmd: Project lead; made MOSH/libmosh, devfw & MPkeys manager, a bunch of small changes.${N}
-${Y}lxrd: Discovered policy-test-tool and created device policy editing script, made a script to let us update ChromeOS on-device.${N}
-\033[38;5;126mkxtzownsu: Did code review to make sure we weren't skidding until he stepped down.${N}
+${R}mariahscarycarey: ${P}Lead developer; made image builder, device policy editor frontend, ChromeOS version switcher, did most bugfixing, and MANY small changes to other code.${N}
+\033[38;5;78mdmd: Project lead; made MOSH/libmosh, devfw & MPkeys manager, base ChromeOS updater, and a bunch of small changes.${N}
+${Y}lxrd: Discovered policy-test-tool and created device policy editing script, made a script to let us stream ChromeOS updates.${N}
+\033[38;5;126mkxtzownsu: Did code review to make sure we weren't skidding until he stepped down [05-26-2026].${N}
 \033[38;5;93mxz8f: Helped with custom bootsplashes.${N}
 \033[38;5;94mcon: emotional support (also helped with minor bugs in image downloader)${N}
 \033[38;5;51mCasper1051, \033[38;5;93mMoonstone, \033[38;5;57mpilgorr${N}: creating the default bootsplashes.
