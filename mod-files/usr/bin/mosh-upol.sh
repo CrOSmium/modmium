b59e9ae6e3150da402efbf5dc21367f1419cc1d0
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-06-03 18:00:24 -0500

    chromeos-setdevpasswd no longer causes problems for this

--- mod-files/usr/bin/mosh-upol.sh
+++ mod-files/usr/bin/mosh-upol.sh
@@ -56,7 +56,6 @@ ${G}+##############################################+
 | -------------------------------------------- |
 | Allows policy changes above 131              |
 +##############################################+
-${R}THIS WILL NOT WORK IF YOU'VE RUN 'chromeos-setdevpasswd'${G}
 ${B}Run this *before* signing into the target email. ${N}
 If it's already logged in, remove the account, you can do this by rebooting, then clicking the drop-down by its pfp and pressing ${R}\"Remove account\"${N} or powerwashing if your enterprise has a custom signin screen with no delete account option.
 also, make sure you're connected to the internet before running this.
