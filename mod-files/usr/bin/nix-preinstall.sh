6c6ef4324b42b5a220fa2015460ff967cea9c9e9
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-06-04 04:39:52 -0500

    fix indentation

--- mod-files/usr/bin/nix-preinstall.sh
+++ mod-files/usr/bin/nix-preinstall.sh
@@ -10,36 +10,36 @@ MARKER="/usr/local/.nix_install_done"
 tput civis # :whale:
 
 installNix(){
-	runscript /usr/bin/.nix-install.sh
+  runscript /usr/bin/.nix-install.sh
 }
 
 mixupd(){
-	echo -e "Updating mix..."
-	sudo cp /usr/bin/.mix /usr/bin/mix
-	sleep 1
-	echo -e "Done!"
-	sleep 0.6
+  echo -e "Updating mix..."
+  sudo cp /usr/bin/.mix /usr/bin/mix
+  sleep 1
+  echo -e "Done!"
+  sleep 0.6
 }
 
 updateMix(){
-	runscriptnoroot mixupd
+  runscriptnoroot mixupd
 }
 
 menu_reset(){
-	if [[ -f $MARKER ]]; then
-	options=("Install Nix" "Update ${G}Mix${N}" "Go Back")
-	functions=("installNix" "updateMix" "quit")
-	else
-	options=("Install Nix" "Go Back")
-	functions=("installNix" "quit")
-	fi
-	num_options=${#options[@]}
-	menuText=$(cat <<EOF
+  if [[ -f $MARKER ]]; then
+  options=("Install Nix" "Update ${G}Mix${N}" "Go Back")
+  functions=("installNix" "updateMix" "quit")
+  else
+  options=("Install Nix" "Go Back")
+  functions=("installNix" "quit")
+  fi
+  num_options=${#options[@]}
+  menuText=$(cat <<EOF
 This will install 'Nix', A package manager usable on Modmium, ${R}Not recommended unless you know what you're doing.${N}
 You can use '${B}mix${N} [arg]' (a command wrapper) in a root shell to use Nix like a regular package manager like apt if you're lazy.\n
 EOF
-	)
-	num_options=${#options[@]}
+  )
+  num_options=${#options[@]}
 }
 
 menu_reset
