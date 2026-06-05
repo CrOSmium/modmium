ff491444c0cc95472e6b1ab1b461a07c4d08bb5b
xXMariahScaryXx <219577531+xXMariahScaryXx@users.noreply.github.com>
2026-06-04 19:09:41 -0500

    add warning about nix instability

--- mod-files/usr/bin/nix-preinstall.sh
+++ mod-files/usr/bin/nix-preinstall.sh
@@ -34,7 +34,9 @@ menu_reset(){
   functions=("installNix" "quit")
   fi
   num_options=${#options[@]}
+  [[ $VERSION -gt 140 ]] && WARNING="${R}Nix installation is known to be unstable on versions above 140.${N} Proceed at your own risk."
   menuText=$(cat <<EOF
+$WARNING
 This will install 'Nix', A package manager usable on Modmium, ${R}Not recommended unless you know what you're doing.${N}
 You can use '${B}mix${N} [arg]' (a command wrapper) in a root shell to use Nix like a regular package manager like apt if you're lazy.\n
 EOF
