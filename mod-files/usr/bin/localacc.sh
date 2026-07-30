46e0820ab389c5d3cf1ef5f0e9bc26c828fcc137
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-07-30 17:30:22 -0500

    add extra warning

--- mod-files/usr/bin/localacc.sh
+++ mod-files/usr/bin/localacc.sh
@@ -212,7 +212,6 @@ GR=$'\033[38;5;46m'
 echo -e "\n${GR}Done! '$U' has been added as a local account${RE} \nIf logging in doesn't work, remove the account (or powerwash) and try again."
 if [[ "$SKIP_OOBE" -eq 1 ]]; then
   initctl restart ui
-
   echo "UI restarted. Exit out of VT to continue..."
 
   while true; do
@@ -220,7 +219,8 @@ if [[ "$SKIP_OOBE" -eq 1 ]]; then
 
     if [[ "$cur" != "vt1" && "$cur" != "vt2" && "$cur" != "vt3" ]]; then
       echo "Detected $cur, restarting UI again..."
-      sleep 3
+      echo -e "If you are still in the oobe once this menu closes, open a root shell and type 'initctl restart ui'"
+      sleep 4
       initctl restart ui
       break
     fi
