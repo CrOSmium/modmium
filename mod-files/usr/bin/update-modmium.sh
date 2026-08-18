8c0171470b77aa38ab59a4dd203de422400232fd
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-08-17 22:16:38 -0500

    no way i swapped && with || on accident :sob:

--- mod-files/usr/bin/update-modmium.sh
+++ mod-files/usr/bin/update-modmium.sh
@@ -445,7 +445,7 @@ else
 fi
 menu_reset() {
   menuText="\nModmium Manager\n"
-  [[ $unconverted_fs -eq 1 ]] || menuText="\nModmium Manager\n -- ${Y}You are running Modmium on ${R}ext2${Y}, the next time you version switch, you will be upgraded to ${G}ext4${N} --"
+  [[ $unconverted_fs -eq 1 ]] && menuText="\nModmium Manager\n -- ${Y}You are running Modmium on ${R}ext2${Y}, the next time you version switch, you will be upgraded to ${G}ext4${N} --"
   options=("Update Modmium" "Change ChromeOS Version" "Swap Boot Priority" "Toggle Enrollment" "Add Local Account" "Feature Toggles" "Exit")
   functions=("updateModmium" "installCros" "toggleBootPriority" "toggleEnrollment" "localAcc" "features" "quit")
   num_options=${#options[@]}
