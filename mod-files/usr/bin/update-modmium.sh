2889c8b398bea22a45407faa9ecb2075c78b9bc5
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-09-01 21:59:28 -0500

    removed for now

--- mod-files/usr/bin/update-modmium.sh
+++ mod-files/usr/bin/update-modmium.sh
@@ -83,7 +83,6 @@ askBranch(){
 }
 
 dropModFiles() {
-  rm -f /usr/sbin/device_managementd # cp refuses to write over it if it exists and is running
   modFiles=$(find modmium/mod-files -mindepth 1 -name "*")
   for file in $modFiles; do
     if [[ -d $file ]]; then
