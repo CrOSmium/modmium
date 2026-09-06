767b5cfcf735a801e70b7bc6b5c0d7d084aa9b98
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-09-06 00:52:44 -0500

    fix /root/.modshell not found

--- mod-files/usr/lib/libmosh.sh
+++ mod-files/usr/lib/libmosh.sh
@@ -15,7 +15,7 @@ selected_index=0
 branch=$(cat /.branch)
 modver=$(cat /usr/share/.version)
 shellfile=/root/.modshell
-shell=$(cat $shellfile)
+shell=$(cat "$shellfile" 2>/dev/null || echo "bash")
 # -----------------------
 
 # TUI colors :D
