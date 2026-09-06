f13e58f23ee038dcb7df278422fb41a414e5f4e9
DMDCR <111153358+DMDCR@users.noreply.github.com>
2026-09-05 23:21:15 -0500

    same here

--- mod-files/usr/bin/change-shell.sh
+++ mod-files/usr/bin/change-shell.sh
@@ -45,7 +45,7 @@ menu_reset(){
   functions=("changeShell" "quit")
   num_options=${#options[@]}
   menuText=$(cat <<EOF
-Your current shell is ${B}$(basename ${shell:-bash})${N}
+\nYour current shell is ${B}$(basename ${shell:-bash})${N}
 You can change what shell will be used for root in this menu.
 Ensure the shell is in \$PATH (nix-installed shells are supported).\n
 EOF
