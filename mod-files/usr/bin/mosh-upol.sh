f0c309fb15dfaef18fcae05822bd947bc1684036
Unixity <103908889+Unixityyy@users.noreply.github.com>
2026-09-15 07:06:05 -0500

    forgot to remove the todo

--- mod-files/usr/bin/mosh-upol.sh
+++ mod-files/usr/bin/mosh-upol.sh
@@ -78,9 +78,7 @@ EOF
   echo -ne "${G}Install uBlock Origin MV3? [Y/n]: ${N}"
   read -r install_ublock
 
-  recommend_force_ids=("johiffgefcnfiddcakohlcpebgpidnji" "hkobaiihndnbfhbkmjjfbdimfbdcppdh") # TODO: add the id for Securly Classroom
-														   # i think this would also fix issue #58
-
+  recommend_force_ids=("johiffgefcnfiddcakohlcpebgpidnji" "hkobaiihndnbfhbkmjjfbdimfbdcppdh") # i think this would also fix issue #58
   found_ids=()
   for id in "${recommend_force_ids[@]}"; do
     if grep -q "$id" /root/policy.json; then
